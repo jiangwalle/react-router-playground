3e72102c35eeb41eb417473ff20626f8b3cab59f
wallejiang <jiangwalle@gmail.com>
2018-04-14 16:25:14 +1200

    Rename Movie component

--- src/routes/Movie.js
+++ src/routes/Movie.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-export default class Movies extends Component {
+export default class Movie extends Component {
   render() {
     const { match } = this.props;
 
